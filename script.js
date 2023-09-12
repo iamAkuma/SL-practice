24698460b82539ba935d0627dca82d7dd4264de8
Saral Bajracharya <rmanbajra@gmail.com>
2023-09-12 16:37:32 +0545

    Form validation done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const button = document.getElementById('btn');
-button.addEventListener('click', function() {
-    console.log('Button clicked')
-    alert('Button clicked!')
+// const button = document.getElementById('btn');
+// button.addEventListener('click', function() {
+//     console.log('Button clicked')
+//     alert('Button clicked!')
 
-})
+// })
 
 let todayDate = new Date()
 document.write(todayDate.toDateString() + '<br>')
@@ -43,3 +43,32 @@ greet("akuma");
 
 document.getElementById("content").innerHTML = "New content" + '<br>';
 document.getElementById("content").textContent = "Updated text";
+
+
+// Get references to the HTML elements
+const nameField = document.getElementById("nameField");
+const passwordField = document.getElementById("passwordField");
+const loginButton = document.getElementById("btn");
+
+// Add an event listener to the login button
+loginButton.addEventListener("click", function(event) {
+    // Prevent the form from submitting
+    event.preventDefault();
+
+    // Validate the name field
+    const nameValue = nameField.value.trim(); // Remove leading/trailing spaces
+    if (nameValue === "") {
+        alert("Name field cannot be empty");
+        return; // Stop further execution
+    }
+
+    const passwordValue = passwordField.value.trim();
+    if(passwordValue===""){
+        alert('Password Field Cannot Be Empty');
+        return;
+    }
+
+    // If the name is valid, you can proceed with your login logic here
+    // For now, let's just display an alert
+    alert("Login Success!")
+});
